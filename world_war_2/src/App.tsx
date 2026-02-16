750492dce1e4f281ee3d44e1246873af75617d8f
Didula Jeewandara <didulaj.23@cse.mrt.ac.lk>
2026-02-16 21:51:20 +0530

    first page of my new website

---
+++ world_war_2/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+
+function App() {
+
+  return (
+
+    <section className="first_page">
+      <div className="Name">
+        <h1 className='Topic'>Scar of Modern History</h1>
+        <h2 className='Topic2'>World War II</h2>
+      </div>
+
+      <div className="pages" >
+        <button className='timeline' onClick={() => {}}>Timeline</button>
+        <button className='countries' onClick={() => {}}>Countries</button>
+        <button className="theaters" onClick={() => {}}>Theaters</button>
+      
+      </div>
+
+    </section>)
+}
+
+export default App
