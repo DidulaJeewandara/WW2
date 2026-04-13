e7676c40745e3988bb513b4ef605c7686338ac75
Didula Jeewandara <didulaj.23@cse.mrt.ac.lk>
2026-04-13 18:08:37 +0530

    Remove unused Link import from App.tsx

--- world_war_2/src/App.tsx
+++ world_war_2/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom'
 import Technical from './components/technnical.tsx'
 
 
